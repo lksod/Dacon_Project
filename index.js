1dd922a9f51f6c78bc714f34ad7076751aac699d
Lucas O. Damaceno <68448724+lksod@users.noreply.github.com>
2021-05-20 10:44:20 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+function init() {
+    var n = 10;
+    var n1 = 20;
+    var s = 10 + 20;
+    console.log(s);
+}
+
+
+function botao () {
+    alert ("Funcionou!"); 
+    var text = document.getElementById("texto");
+    
+}
